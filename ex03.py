54e10c195d0afdecdb039965f776ef43b39b65b1
Ânderson Mendes <156342039+Adsmendees11@users.noreply.github.com>
2025-08-19 11:21:17 -0300

    Update and rename ex17.py to ex03.py

--- ex03.py
+++ ex03.py
@@ -3,4 +3,5 @@
 produto = float (input('Valor do produto:'))
 desconto = produto - (produto * 5 / 100)
 print ('O produto do valor R${:.2f} com 5% de desconto fica por:R${:.2f}'.format(produto,desconto))
+
 # {:.2f} O 2f em Python é uma especificação de formatação que indica que o valor deve ser apresentado como um float com duas casas decimais 
