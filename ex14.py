a9c92c3f8b51b472f66e7e3d58614bec9621a55a
Ânderson Mendes <156342039+Adsmendees11@users.noreply.github.com>
2025-08-19 11:32:52 -0300

    Add files via upload

---
+++ ex14.py
@@ -0,0 +1,5 @@
+# Faça um programa que coverta metros para centímetros
+
+m = int(input('Valor em metros:'))
+c = (m*100)
+print ('Podemos dizer que {} metros equivale a {} centímetros'.format(m,c))
